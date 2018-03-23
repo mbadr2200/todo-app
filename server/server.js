ef4d576d558291107c1f1ff80d93e5684d18d865
mostafa <mostafa_b2200@yahoo.com>
2018-03-23 13:47:00 +0200

    added update route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // Libraries
+const _ = require('lodash');
 const express =  require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} =require('mongodb');
@@ -87,6 +88,39 @@ app.delete('/todos/:id',(req,res) => {
 
 })
 
+
+ app.patch('/todos/:id',(req,res) => {
+     var id = req.params.id;
+     var body = _.pick(req.body,['text','completed']);
+
+     if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+        body.completedAt = new Date().getTime();
+    }else
+    {
+        body.completed= false;
+        body.completedAt=null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body} , {new:true})
+    .then((todo) => {
+        
+        if(!todo){
+           return res.status(404).send();
+        }
+
+        res.status(200).send(todo);
+    })
+    .catch((e) => {
+        res.status(400).send();
+    });
+
+
+ });
 
 // listen to port
 app.listen(port,() =>console.log(`Server Connected to port ${port}...`));
