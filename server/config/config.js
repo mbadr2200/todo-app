f9e87d2af3813dd09f5369e182adbdceaab9d9d1
mostafa <mostafa_b2200@yahoo.com>
2018-03-23 14:57:19 +0200

    last try

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,4 @@ if(env === 'development')
 {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}else if (env === 'production')
-{
-    process.env.MONGODB_URI = "mongodb://admin:123@ds119049.mlab.com:19049/todo-app";
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.promise = global.promise;
 // mongoose.connect('mongodb://admin:123321@ds115579.mlab.com:15579/orders');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI||'mongodb://admin:123@ds119049.mlab.com:19049/todo-app');
 
 
 module.exports.mongoose = mongoose ;
