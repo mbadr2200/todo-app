1c1efed82792d6dc093ef84d3ff3f216f7afaac0
mostafa <mostafa_b2200@yahoo.com>
2018-03-24 18:37:29 +0200

    - hash password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,13 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
 // var message = 'iam user num 3';
 // var hashed_message = SHA256(message);
 
 
-var data ={
-    id:4
-}
+// var data ={
+//     id:4
+// }
 
 // var token = {
 //     data,
@@ -23,6 +23,22 @@ var data ={
 //     console.log('data changed !')
 // }
 
-var token = jwt.sign(data,'theSecret');
+// var token = jwt.sign(data,'theSecret');
 
-var decoded = jwt.verify(token,'theSecret');
+// var decoded = jwt.verify(token,'theSecret');
+
+
+
+var password = 'thisispassword';
+
+bcrypt.genSalt(10,(err,salt) => {
+     bcrypt.hash(password,salt,(err,hash) => {
+        // console.log(hash);
+    });
+});
+
+var hashed_password = '$2a$10$v7GQcquv6AQqRQkvAMmz7uyfHxP0SjI0uWlFvCv.zR6zWstfBIqrW';
+
+bcrypt.compare(password,hashed_password,(err,result) => {
+    console.log(result);
+})
