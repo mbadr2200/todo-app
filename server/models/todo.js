f8fd5eb6cef504755ba0d55150a5fa3a8f56495e
mbadr2200 <mostafa_b2200@yahoo.com>
2022-12-16 13:40:07 +0200

    feat: add creator to the todo

--- server/models/todo.js
+++ server/models/todo.js
@@ -14,6 +14,10 @@ var Todo = mongoose.model('Todo' , {
     completedAt:{
         type:Number,
         defualt:null  
+    },
+    _creator:{
+        required:true,
+        type:mongoose.Schema.Types.ObjectId
     }
 });
 
